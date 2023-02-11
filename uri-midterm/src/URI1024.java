4a76528bc068f4782036444c8fde5922e9ffdc46
mehry_s@auca.kg <A699415j>
2023-02-11 14:12:21 +0600

    URI-midterm (100%)

---
+++ uri-midterm/src/URI1024.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class URI1024 {
+
+public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        int N = reader.nextInt();
+
+
+        for (int i = 0; i < N; i++) {
+
+
+        StringBuilder M = new StringBuilder(readLine(reader));
+
+
+        for (int j = 0; j < M.length(); j++) {
+        int code = M.charAt(j);
+        if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        char c = (char) (code + 3);
+        M.setCharAt(j, c);
+        }
+        }
+
+
+        M.reverse();
+
+
+        for (int j = M.length() / 2; j < M.length(); j++) {
+        char c = (char) (M.charAt(j) - 1);
+        M.setCharAt(j, c);
+        }
+
+
+        System.out.println(M.toString());
+        }
+        }
+
+
+public static String readLine(Scanner reader) {
+        String line = reader.nextLine();
+        while (line.isEmpty())
+        line = reader.nextLine();
+        return line;
+        }
+
+        }
---
+++ uri-midterm/src/URI1238.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class URI1238 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        Locale.setDefault(new Locale("en", "US"));
+
+        int N = Integer.parseInt(sc.nextLine());
+
+        for (int i=0 ; i<N ; i++) {
+            String[] prohabtid = sc.nextLine().split(" ");
+            char[] fr1 = prohabtid[0].toCharArray();
+            char[] fr2 = prohabtid[1].toCharArray();
+            String sale = "";
+
+            for (int z=0 ; z<(fr1.length+fr2.length) ; z++) {
+                if (z<fr1.length)
+                    sale+=fr1[z];
+                if (z<fr2.length)
+                    sale+=fr2[z];
+            }
+            System.out.println(sale);
+        }
+        sc.close();
+    }
+}
