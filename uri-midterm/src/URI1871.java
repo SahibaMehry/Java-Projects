01d5ca8ef570f186980ceddc0a88accd20499eac
mehry_s@auca.kg <A699415j>
2023-03-12 15:16:54 +0600

    URI1871 Accepted

---
+++ uri-midterm/src/URI1871.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class URI1871{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m, n;
+        String s;
+
+        while(in.hasNextLine()) {
+            m = in.nextInt(); n = in.nextInt();
+
+            if(m == 0 && n == 0) {
+                break;
+            }
+
+            s = Integer.toString(m + n);
+            s = s.replace("0", "");
+            System.out.println(s);
+        }
+    }
+}
