c06c25943c9588a9a392f01be5f39d671aefe8fd
mehry_s@auca.kg <A699415j>
2023-03-19 15:07:32 +0600

    URI1873 Accepted

--- uri-midterm/src/URI1873.java
+++ uri-midterm/src/URI1873.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class URI1837{
+public class URI1873 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
